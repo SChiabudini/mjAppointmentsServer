d530d5a6e38cd4373ad55b33cbe3834d77161cc0
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-11-07 20:17:28 -0300

    get vechicles done

--- src/routes/vehicleRoutes.js
+++ src/routes/vehicleRoutes.js
@@ -1,15 +1,15 @@
 const vehicleRouter = require('express').Router();
-const { postVehicleHandler } = require('../handlers/vehicleHandlers/indexHandlers.js');
+const { getVehiclesHandler, postVehicleHandler } = require('../handlers/vehicleHandlers/indexHandlers.js');
 
-//vehicleRouter.get('/', (req, res, next) => {
+vehicleRouter.get('/', (req, res, next) => {
 //    const { licensePlate } = req.query;
 //
 //    if (licensePlate) {
 //        return getVehiclesByLicensePlateHandler(req, res, next);
 //    };
 //
-//    return getVehiclesHandler(req, res, next);
-//});
+    return getVehiclesHandler(req, res, next);
+});
 
 vehicleRouter.post('/', postVehicleHandler);
 
