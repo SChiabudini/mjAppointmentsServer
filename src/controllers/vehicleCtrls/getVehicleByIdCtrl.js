c862786e9d076ca194bd0c130aa94350401101c0
Matías Morales <programacionfrida@gmail.com>
2024-12-26 20:23:10 -0300

    Detail vehicle ok

--- src/controllers/vehicleCtrls/getVehicleByIdCtrl.js
+++ src/controllers/vehicleCtrls/getVehicleByIdCtrl.js
@@ -5,8 +5,23 @@ const getVehicleByIdCtrl = async (_id) => {
 
     if(_id) {
         const vehicleById = await Vehicle.findOne({ _id })
-        .populate('personClient')
-        .populate('companyClient');
+        .populate({
+            path: 'personClient',  
+            select: 'dni name email phones cuilCuit'
+        })
+        .populate({
+            path: 'companyClient',  
+            select: 'cuit name email phones address'
+        })
+        .populate({
+            path: 'serviceSheets',  
+            select: 'date kilometers kmsToNextService oil filters notes amount number'
+        })
+        .populate({
+            path: 'mechanicalSheets',  
+            select: 'date kilometers keyWords description amount number'
+        });
+
         return vehicleById;
     }
 
