576e51c58cd356e6d5e8f42961abd8ac8d5bd4d5
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-01-19 20:13:09 -0300

    populate all info at client by id

--- src/controllers/companyClientCtrls/getCompanyClientByIdCtrl.js
+++ src/controllers/companyClientCtrls/getCompanyClientByIdCtrl.js
@@ -5,26 +5,9 @@ const getCompanyClientByIdCtrl = async (_id) => {
 
   if (_id) {
       const companyClientById = await CompanyClient.findOne({ _id })
-      .populate({
-        path: 'vehicles',  
-        select: 'brand engine licensePlate model year'
-      })
-      .populate({
-        path: 'serviceSheets',  
-        select: 'date vehicle',  
-        populate: {  
-          path: 'vehicle',
-          select: 'licensePlate'  
-        }
-      })
-      .populate({
-        path: 'mechanicalSheets',  
-        select: 'date vehicle',
-        populate: {
-          path: 'vehicle',
-          select: 'licensePlate'
-        }
-      });
+      .populate('mechanicalSheets')
+      .populate('serviceSheets')
+      .populate('vehicles');
   
       return companyClientById;
     };
--- src/controllers/personClientCtrls/getPersonClientByIdCtrl.js
+++ src/controllers/personClientCtrls/getPersonClientByIdCtrl.js
@@ -5,26 +5,9 @@ const getPersonClientByIdCtrl = async (_id) => {
 
   if (_id) {
     const personClientById = await PersonClient.findOne({ _id })
-    .populate({
-      path: 'vehicles',  
-      select: 'brand engine licensePlate model year'
-    })
-    .populate({
-      path: 'serviceSheets',  
-      select: 'date vehicle',  // Poblar la propiedad `vehicle` que es un ID
-      populate: {  // Ahora poblamos la propiedad `vehicle` para que contenga los detalles completos del vehículo
-        path: 'vehicle',
-        select: 'licensePlate'  // Solo seleccionamos la patente (puedes agregar más propiedades si lo necesitas)
-      }
-    })
-    .populate({
-      path: 'mechanicalSheets',  
-      select: 'date vehicle',
-      populate: {
-        path: 'vehicle',
-        select: 'licensePlate'
-      }
-    });
+    .populate('mechanicalSheets')
+    .populate('serviceSheets')
+    .populate('vehicles');
 
     return personClientById;
   };
