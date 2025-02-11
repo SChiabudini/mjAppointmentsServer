255fa18a160ef61468f7ce5d6f73636b1b7a8765
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-02-11 08:36:58 -0300

    email info fixed

--- src/jobs/notifications/serviceReminderEmail.js
+++ src/jobs/notifications/serviceReminderEmail.js
@@ -31,7 +31,7 @@ const serviceReminderEmail = async (sgMail) => {
             <li>Vehículo: <b>${sheet.vehicle.licensePlate}</b></li>
             <li>Aceite: ${sheet.oil}</li>
             <li>Filtros: ${sheet.filters.length > 0 ? sheet.filters.join(', ') : "Ninguno"}</li>
-            <li>Kilómetraje: ${sheet.kilometers}</li>
+            <li>Kilometraje: ${sheet.kilometers}</li>
             <li>Kilómetros para el próximo service: ${sheet.kmsToNextService}</li>
         `;
 
