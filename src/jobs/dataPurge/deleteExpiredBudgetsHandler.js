c7c86d2623502b044cc022eab4eb601a08d03264
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-02-10 21:13:10 -0300

    delete expired budgets done

---
+++ src/jobs/dataPurge/deleteExpiredBudgetsHandler.js
@@ -0,0 +1,18 @@
+const deleteExpiredBudgetsCtrl = require('../../controllers/budgetCtrls/deleteExpiredBudgetsCtrl.js');
+
+const deleteExpiredBudgetsHandler = async () => {
+
+    try {
+
+        const today = new Date().toISOString();
+
+        const expiredBudgets = await deleteExpiredBudgetsCtrl(today);
+
+        console.log(`${expiredBudgets} budgets have been deleted.`);
+    } catch (error) {
+        console.error('Error deleting expired budgets:', error);
+        throw error;
+    }
+};
+
+module.exports = deleteExpiredBudgetsHandler;
